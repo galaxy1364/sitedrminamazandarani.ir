a555a2d4da043ffbcf3d4813243ef623a379c3e0
onspacegalaxy.mehdi.m <galaxy.mehdi1364@gmail.com>
2026-09-08 11:44:55 -0700

    feat: load premium visual layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./sections.css";
+import "./premium.css";
 import { site } from "@/lib/site";
 
 const productionUrl = "https://sitedrminamazandarani.ir";
