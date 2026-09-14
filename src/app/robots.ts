91935db5462134087a6954639669162d1b4fa9f6
onspacegalaxy.mehdi.m <galaxy.mehdi1364@gmail.com>
2026-09-14 04:19:48 -0700

    seo: make robots metadata follow verified site URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,4 +1,5 @@
 import type { MetadataRoute } from "next";
+import { canonicalDomainConfigured, siteUrl } from "@/lib/seo";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -6,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: "https://sitedrminamazandarani.ir/sitemap.xml",
-    host: "https://sitedrminamazandarani.ir"
+    sitemap: `${siteUrl}/sitemap.xml`,
+    ...(canonicalDomainConfigured ? { host: siteUrl } : {})
   };
 }
